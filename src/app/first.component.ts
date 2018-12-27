3447d905338325542d667fc9f996ed89b5ea1822
Harshdeep Bilaiya <harshdeep.bilaiya@globant.com>
2018-12-27 15:24:14 +0530

    initial commit

---
+++ src/app/first.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  template: `<div>
+              <h2>First Component</h2>
+            </div>`
+})
+export class FirstComponent {
+
+}
