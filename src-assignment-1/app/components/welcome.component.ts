2cb3cb46278c6e957c8320b98d1a51f2899b341e
Harshdeep Bilaiya <harshdeep.bilaiya@globant.com>
2019-01-03 17:47:36 +0530

    directory cleanup

--- src-assignment-1/app/components/welcome.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-welcome-component',
-  template: `<h2>Welcome to {{ trainingName }}!</h2>`
-})
-export class WelcomeComponent {
-  trainingName = 'Angular Training';
-}
