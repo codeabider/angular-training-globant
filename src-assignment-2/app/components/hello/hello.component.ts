23a0b718de25bfddab2eb354d9d9046ae20d09fd
Harshdeep Bilaiya <harshdeep.bilaiya@globant.com>
2019-01-03 17:44:23 +0530

    directory cleanup

--- src-assignment-2/app/components/hello/hello.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-hello-component',
-  template: `<h2>Hello {{ name }}!!</h2>`
-})
-export class HelloComponent {
-  name = 'Harshdeep';
-}
--- src-assignment-day2/app/interfaces/person.ts
+++
@@ -1,8 +0,0 @@
-interface Person {
-  id: number;
-  name: string;
-  age: number;
-  address: string;
-  email: string;
-  isGraduate: boolean;
-}
